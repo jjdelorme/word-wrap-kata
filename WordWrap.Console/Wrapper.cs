f3d1a963d81eb53dfb1b14570b4279402fde3e6d
Jason De Lorme <jasondel@google.com>
2025-04-04 08:49:58 -0600

    Additional tests and refactor into instance class.

--- WordWrap.Console/Wrapper.cs
+++ WordWrap.Console/Wrapper.cs
@@ -2,11 +2,55 @@ namespace WordWrap.Library;
 
 public class Wrapper
 {
+    int lineLength;
+
+    public Wrapper(int lineLength)
+    {
+        this.lineLength = lineLength;
+    }
+
     public static string Wrap(string text, int lineLength)
     {
-        if (text?.Length < lineLength)
-            return text;
-        else
+        return new Wrapper(lineLength).Wrap(text);
+    }
+
+    public string Wrap(string text)
+    {
+        if (lineLength <= 0)
+            throw new ArgumentOutOfRangeException("lineLength", "Must be greater than 0.");
+
+        if (string.IsNullOrEmpty(text))
             return "";
+
+        text = text.Trim();
+
+        if (text.Length <= lineLength)
+        {
+            return text;
+        }
+        else
+        {
+            return BreakLine(text);
+        }
+    }
+
+    private string BreakLine(string text)
+    {
+        int breakPoint = GetBreakPoint(text);
+
+        string line = text.Substring(0, breakPoint).Trim();
+
+        return line + '\n' +
+            Wrap(text.Substring(breakPoint));
+    }
+
+    private int GetBreakPoint(string text)
+    {
+        int space = text.LastIndexOf(' ');
+        
+        if (space > 0)
+            return space;
+        else
+            return lineLength;
     }
 }
--- WordWrap.Tests/WrapperTests.cs
+++ WordWrap.Tests/WrapperTests.cs
@@ -17,8 +17,47 @@ public class WrapperTests
     }
 
     [Fact]
+    public void Wrap0LineLengthShouldThrowException()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => Wrapper.Wrap("hello", 0));
+    }
+
+    [Fact]
     public void OneShortWordDoesNotWrap()
     {
         Assert.Equal("hello", Wrapper.Wrap("hello", 10));
     }
+
+    [Fact]
+    public void OneWordLongerThanLimitShouldWrap()
+    {
+        Assert.Equal("long\nword", Wrapper.Wrap("longword", 4));
+        Assert.Equal("longer\nword", Wrapper.Wrap("longerword", 6));
+    }
+
+    [Fact]
+    public void WordLongerThanTwiceLengthShouldBreakTwice()
+    {
+        Assert.Equal("very\nlong\nword", Wrapper.Wrap("verylongword", 4));
+    }
+
+    [Fact]
+    public void TwoWordsLongerThanLimitShouldWrap()
+    {
+        Assert.Equal("word\nword", Wrapper.Wrap("word word", 6));
+        Assert.Equal("wrap\nhere", Wrapper.Wrap("wrap here", 6));
+    }
+
+    [Fact]
+    public void DoubleSpaceShouldTrim()
+    {
+        Assert.Equal("more\nspace", Wrapper.Wrap("more  space", 5));
+    }
+
+    [Fact]
+    public void ThreeWordsJustOverTheLimitShouldWrapAtSecondWord()
+    {
+        Assert.Equal("word word\nword", Wrapper.Wrap("word word word", 9));
+    }
+
 }
